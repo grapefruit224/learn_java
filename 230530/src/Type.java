c8ec9301174de14d39126e88440f0e95459e60c5
grapefruit224 <prikey7@gmail.com>
2024-05-30 17:53:03 +0900

    init

---
+++ 230530/src/Type.java
@@ -0,0 +1,36 @@
+public class Type {
+  public static void main(String[] args) {
+
+      float myFloat = 9.75f;
+      float anotherFloat = myFloat * 2;
+
+      System.out.println(myFloat);
+      System.out.println(anotherFloat);
+
+
+
+      // short 타입
+//      short sVal = 10;
+//      short result = (short)(sVal + 10); // 형 변환
+//      System.out.println(result);
+
+      // char 타입
+//      char c1 = 'A';
+//      char c2 = 65;
+//      char c3 = '\u0041';
+//
+//      char c4 = '가';
+//      char c5 = 44032;
+//      char c6 = '\uac00';
+//
+//      int uniCode = c1;
+//
+//      System.out.println(c1);
+//      System.out.println(c2);
+//      System.out.println(c3);
+//      System.out.println(c4);
+//      System.out.println(c5);
+//      System.out.println(c6);
+//      System.out.println(uniCode);
+  }
+}
