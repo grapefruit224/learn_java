58022eafc636e072bcd55ce5253b986c2e3a79d3
grapefriut422 <prikey7@gmail.com>
2024-06-17 17:54:19 +0900

    230617 commit!!

---
+++ 240617/src/Box.java
@@ -0,0 +1,14 @@
+public class Box<T> {
+    private T t;
+
+    public void set(T t){
+        this.t = t;
+    }
+
+    public T get(){
+        return t;
+    }
+    public boolean isIns(){
+        return t instanceof String;
+    }
+}
---
+++ 240617/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Box<String> box = new Box<>();
+        box.set("hello");
+
+        String str = box.get();
+        System.out.println(str);
+
+        Box<Integer> box2 = new Box<>();
+        box2.set(9);
+
+        int value = box2.get();
+        System.out.println(value);
+    }
+
+}
